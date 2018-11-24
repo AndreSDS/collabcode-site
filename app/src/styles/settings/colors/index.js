858213037e1ae48491dbb7bcd7074d33300db858
AndréSDS <andre_smiths@outlook.com>
2018-11-24 16:32:38 -0200

    finalizado header

--- app/src/styles/settings/colors/index.js
+++ app/src/styles/settings/colors/index.js
@@ -3,6 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 const Colors = createGlobalStyle`
     :root{
       --color-negative: #0e1d32;
+      --color-negative-dark: #09121e;
       --color-zero: #fff;
     }
 `;
